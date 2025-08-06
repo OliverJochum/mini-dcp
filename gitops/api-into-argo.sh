4bc51072fca6c40b0d836ca2a73e7e653bade324
Oliver Jochum <oliver.jochum@lufthansa-group.com>
2025-08-06 11:52:44 +0200

    Push to main

--- gitops/api-into-argo.sh
+++ gitops/api-into-argo.sh
@@ -1,3 +1,5 @@
 argocd admin initial-password -n argocd
 argocd login localhost:8080 --insecure --username admin --password $(kubectl get secret -n argocd argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 kubectl config set-context --current --namespace=argocd
+argocd repo add https://github.com/OliverJochum/mini-dcp.git
+argocd app create flightsearch-app --repo https://github.com/OliverJochum/mini-dcp.git --path gitops --dest-server https://kubernetes.default.svc --dest-namespace default
