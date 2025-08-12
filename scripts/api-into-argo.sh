451072ad46dda261cac12f98f9244c9ed895e8cb
Oliver Jochum <oliver.jochum@lufthansa-group.com>
2025-08-12 10:17:40 +0200

    Mark script as deprecated

--- scripts/api-into-argo.sh
+++ scripts/api-into-argo.sh
@@ -1,3 +1,5 @@
+# deprecated, since we can use kubectl apply to create the ArgoCD application directly
+
 argocd admin initial-password -n argocd
 argocd login localhost:8080 --insecure --username admin --password $(kubectl get secret -n argocd argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 kubectl config set-context --current --namespace=argocd
