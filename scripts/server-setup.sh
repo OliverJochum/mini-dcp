36015718f09cf40ea2a63e6ffa5eda3baa252a25
Oliver Jochum <oliver.jochum@lufthansa-group.com>
2025-08-13 15:19:17 +0200

    Create server setup script (init documentation of all commands used)

---
+++ scripts/server-setup.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+sudo apt-get update
+
+# Install kubectl
+printf "Installing kubectl...\n"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+# Install kubeadm
+printf "Installing kubeadm...\n"
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.33/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.33/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+sudo systemctl enable --now kubelet
+
+# Install containerd (CRI)
+printf "Installing containerd...\n"
+apt-get update
+apt-get install -y ca-certificates curl gnupg lsb-release
+
+# (Recommended) Install containerd from Docker’s repo for up-to-date builds
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+  https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+  $(lsb_release -cs) stable" \
+  > /etc/apt/sources.list.d/docker.list
+
+apt-get update
+apt-get install -y containerd.io
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.ipv4.ip_forward = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+sysctl net.ipv4.ip_forward
+
+# Install tigera
+printf "Installing Tigera...\n"
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.30.2/manifests/tigera-operator.yaml
+
+# Install calico 
+printf "Installing Calico...\n"
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.30.2/manifests/custom-resources.yaml
+
+watch kubectl get pods -n calico-system & 
+
+# Remove taint from control-plane node
+printf "Removing taint from control-plane node...\n"
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+
+printf "Installing ArgoCD CLI...\n"
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
+
+printf "Installing ArgoCD...\n"
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+printf "Create NodePort service for ArgoCD...\n"
+kubectl apply -f https://raw.githubusercontent.com/OliverJochum/mini-dcp/main/gitops/argocd-nodeport-svc.yaml
+
+printf "Creating flightsearch-app from manifest file...\n"
+kubectl config set-context --current --namespace=argocd
+kubectl apply -f https://raw.githubusercontent.com/OliverJochum/mini-dcp/main/gitops/flightsearch-app.yaml
+
+
